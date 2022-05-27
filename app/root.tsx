77486bd672464174eb16ab79809d866a1dc2e4e5
ytakhs <21127627+ytakhs@users.noreply.github.com>
2022-05-27 23:29:34 +0900

    Move links and meta to root

--- app/root.tsx
+++ app/root.tsx
@@ -10,12 +10,33 @@ import {
 
 import styles from './styles/app.css';
 
-export const links: LinksFunction = () => [{ rel: 'stylesheet', href: styles }];
+export const links: LinksFunction = () => [
+  { rel: 'stylesheet', href: styles },
+  {
+    rel: 'apple-touch-icon',
+    sizes: '180x180',
+    href: '/images/favicon-apple-touch.png',
+  },
+  { rel: 'icon', href: '/images/favicon-48.png', type: 'image/png' },
+  { rel: 'icon', href: '/images/favicon.svg', type: 'image/svg+xml' },
+  {
+    rel: 'alternate',
+    href: '/feeds/atom.xml',
+    type: 'application/atom+xml',
+  },
+];
 
+const baseUrl = 'https://ytakhs.com';
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
   title: 'ytakhs.com',
   viewport: 'width=device-width,initial-scale=1',
+  'og:title': 'ytakhs.com',
+  'og:description': 'ytakhs.com',
+  'og:type': 'website',
+  'og:url': baseUrl,
+  'og:image': `${baseUrl}/images/og-image.png`,
+  'twitter:card': 'summary',
 });
 
 export default function App() {
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,34 +1,3 @@
-const baseUrl = 'https://ytakhs.com';
-
-export function links() {
-  return [
-    {
-      rel: 'apple-touch-icon',
-      sizes: '180x180',
-      href: '/images/favicon-apple-touch.png',
-    },
-    { rel: 'icon', href: '/images/favicon-48.png', type: 'image/png' },
-    { rel: 'icon', href: '/images/favicon.svg', type: 'image/svg+xml' },
-    {
-      rel: 'alternate',
-      href: '/feeds/atom.xml',
-      type: 'application/atom+xml',
-    },
-  ];
-}
-
-export function meta() {
-  return {
-    title: 'ytakhs.com',
-    'og:title': 'ytakhs.com',
-    'og:description': 'ytakhs.com',
-    'og:type': 'website',
-    'og:url': baseUrl,
-    'og:image': `${baseUrl}/images/og-image.png`,
-    'twitter:card': 'summary',
-  };
-}
-
 export default function Index() {
   return (
     <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>

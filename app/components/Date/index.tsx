c554197efd1a44757d1c685e7185beb60e9fe1d8
ytakhs <21127627+ytakhs@users.noreply.github.com>
2022-05-27 23:02:47 +0900

    Add date component

---
+++ app/components/Date/index.tsx
@@ -0,0 +1,8 @@
+import type { FC } from 'react';
+import { parseISO, format } from 'date-fns';
+
+export const Date: FC<{ dateString: string }> = ({ dateString }) => {
+  const date = parseISO(dateString);
+
+  return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>;
+};

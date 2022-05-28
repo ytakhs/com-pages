1203a632b2338ba2af94c1f927ee8734844c5533
ytakhs <21127627+ytakhs@users.noreply.github.com>
2022-05-28 11:18:07 +0900

    Add top page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,33 +1,64 @@
+import { Link } from '@remix-run/react';
+import { ChevronRightIcon, ExternalLinkIcon } from '@heroicons/react/solid';
+
+import { H2 } from '~/components/Heading';
 import { Layout } from '~/components/Layout';
 
 export default function Index() {
   return (
     <Layout>
-      <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
-        <h1>Welcome to Remix</h1>
+      <h1 className="hidden">ytakhs.com</h1>
+      <div className="flex justify-start">
+        <figure className="block px-2 w-full max-w-[30%] h-full max-h-[30%]">
+          <img
+            className="static rounded-full"
+            src="/images/icon.svg"
+            alt="icon"
+          />
+        </figure>
+        <div className="px-4 m-auto w-full">
+          <H2 style={{ padding: 0 }}>About me</H2>
+          <p>@ytakhs</p>
+        </div>
+      </div>
+      <div className="pt-4">
+        <H2>Links</H2>
         <ul>
-          <li>
-            <a
-              target="_blank"
-              href="https://remix.run/tutorials/blog"
-              rel="noreferrer"
-            >
-              15m Quickstart Blog Tutorial
-            </a>
+          <li className="list-item py-1.5 leading-relaxed">
+            <Link to="/entries">
+              <div className="border-b">
+                <span className="inline-flex items-center">
+                  Writings <ChevronRightIcon className="mx-2 w-5 h-5" />
+                </span>
+              </div>
+            </Link>
           </li>
-          <li>
-            <a
-              target="_blank"
-              href="https://remix.run/tutorials/jokes"
-              rel="noreferrer"
-            >
-              Deep Dive Jokes App Tutorial
-            </a>
+          <li className="list-item py-1.5 leading-relaxed">
+            <Link to="/profile">
+              <div className="border-b">
+                <span className="inline-flex items-center">
+                  Profile <ChevronRightIcon className="mx-2 w-5 h-5" />
+                </span>
+              </div>
+            </Link>
           </li>
-          <li>
-            <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-              Remix Docs
-            </a>
+          <li className="list-item py-1.5 leading-relaxed">
+            <Link to="https://github.com/ytakhs" target="_blank">
+              <div className="border-b">
+                <span className="inline-flex items-center">
+                  GitHub <ExternalLinkIcon className="mx-2 w-5 h-5" />
+                </span>
+              </div>
+            </Link>
+          </li>
+          <li className="list-item py-1.5 leading-relaxed">
+            <Link to="https://twitter.com/ytakhs" target="_blank">
+              <div className="border-b">
+                <span className="inline-flex items-center">
+                  Twitter <ExternalLinkIcon className="mx-2 w-5 h-5" />
+                </span>
+              </div>
+            </Link>
           </li>
         </ul>
       </div>

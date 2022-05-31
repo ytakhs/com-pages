990eff46dd163332dc769f1eefac9457eb7c4e58
ytakhs <21127627+ytakhs@users.noreply.github.com>
2022-05-31 22:52:12 +0900

    Add a script to build entries JSON

--- scripts/buildContent.ts
+++ scripts/buildContent.ts
@@ -1,5 +1,34 @@
-function build() {
-  console.log('foo');
+import { writeFileSync, mkdirSync } from 'fs';
+import { join, dirname } from 'path';
+import { getUnixTime, parseISO } from 'date-fns';
+import { getAllEntries } from '../lib/entry';
+
+async function build() {
+  const entries = getAllEntries();
+  const pwd = process.cwd();
+
+  entries.forEach(
+    ({ title, content, description, createdAt, path: entryPath }) => {
+      const dest = join(pwd, 'public', 'content', `${entryPath}.json`);
+
+      mkdirSync(dirname(dest), { recursive: true });
+      writeFileSync(
+        dest,
+        JSON.stringify({ title, content, description, createdAt })
+      );
+    }
+  );
+
+  const entryMap = entries.map(({ title, path: entryPath, createdAt }) => ({
+    title,
+    path: entryPath,
+    createdTime: getUnixTime(parseISO(createdAt)),
+  }));
+
+  writeFileSync(
+    join(pwd, 'public', 'content', 'entries.json'),
+    JSON.stringify(entryMap)
+  );
 }
 
 build();

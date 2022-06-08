9c406a15bdb94a188b9eaa4d91b3a2f996ecbc8b
ytakhs <21127627+ytakhs@users.noreply.github.com>
2022-06-08 20:51:39 +0900

    Fix sort order of entries

--- app/routes/entries/index.tsx
+++ app/routes/entries/index.tsx
@@ -19,9 +19,9 @@ type LoaderData = {
 const sortEntries = (entries: ReadonlyArray<Entry>): ReadonlyArray<Entry> => {
   return entries.slice().sort(({ createdAt: a }, { createdAt: b }) => {
     if (a < b) {
-      return -1;
-    } else if (a > b) {
       return 1;
+    } else if (a > b) {
+      return -1;
     } else {
       return 0;
     }

ff688271a783b9f59bca1a97a282984f6c8f6780
ytakhs <21127627+ytakhs@users.noreply.github.com>
2022-06-14 22:39:30 +0900

    Modify top page style

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,4 @@
 import { Link } from '@remix-run/react';
-import { ChevronRightIcon, ExternalLinkIcon } from '@heroicons/react/solid';
 
 import { H2 } from '~/components/Heading';
 import { Layout } from '~/components/Layout';
@@ -8,8 +7,8 @@ export default function Index() {
   return (
     <Layout>
       <h1 className="hidden">ytakhs.com</h1>
-      <div className="flex justify-start">
-        <figure className="block px-2 w-full max-w-[30%] h-full max-h-[30%]">
+      <div className="flex justify-start py-8">
+        <figure className="block px-2 m-auto w-full max-w-[30%] h-full max-h-[30%]">
           <img
             className="static rounded-full"
             src="/images/icon.svg"
@@ -18,40 +17,40 @@ export default function Index() {
         </figure>
         <div className="px-4 m-auto w-full">
           <H2 style={{ padding: 0 }}>About me</H2>
-          <p>@ytakhs</p>
+          <ul className="p-4">
+            <li className="tracking-wider leading-relaxed">
+              GitHub:{' '}
+              <a
+                href="https://twitter.com/ytakhs"
+                target="_blank"
+                rel="noreferrer"
+                className="underline"
+              >
+                ytakhs
+              </a>
+            </li>
+            <li className="tracking-wider leading-relaxed">
+              Twitter:{' '}
+              <a
+                href="https://twitter.com/ytakhs"
+                target="_blank"
+                rel="noreferrer"
+                className="underline"
+              >
+                @ytakhs
+              </a>
+            </li>
+          </ul>
+          <H2 style={{ padding: 0 }}>Content</H2>
+          <ul className="p-4">
+            <li className="tracking-wider leading-relaxed">
+              Writings:{' '}
+              <Link to="/entries" className="underline ">
+                /entries
+              </Link>
+            </li>
+          </ul>
         </div>
-      </div>
-      <div className="pt-4">
-        <H2>Links</H2>
-        <ul>
-          <li className="list-item py-1.5 leading-relaxed">
-            <Link to="/entries">
-              <div className="border-b">
-                <span className="inline-flex items-center">
-                  Writings <ChevronRightIcon className="mx-2 w-5 h-5" />
-                </span>
-              </div>
-            </Link>
-          </li>
-          <li className="list-item py-1.5 leading-relaxed">
-            <Link to="https://github.com/ytakhs" target="_blank">
-              <div className="border-b">
-                <span className="inline-flex items-center">
-                  GitHub <ExternalLinkIcon className="mx-2 w-5 h-5" />
-                </span>
-              </div>
-            </Link>
-          </li>
-          <li className="list-item py-1.5 leading-relaxed">
-            <Link to="https://twitter.com/ytakhs" target="_blank">
-              <div className="border-b">
-                <span className="inline-flex items-center">
-                  Twitter <ExternalLinkIcon className="mx-2 w-5 h-5" />
-                </span>
-              </div>
-            </Link>
-          </li>
-        </ul>
       </div>
     </Layout>
   );

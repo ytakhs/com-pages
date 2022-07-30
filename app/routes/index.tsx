9fb4a29d113f7086e7bb45e368bcc428bd1deb68
ytakhs <21127627+ytakhs@users.noreply.github.com>
2022-07-30 13:26:54 +0900

    De-tailwindcss in top page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -6,19 +6,45 @@ import { Layout } from '~/components/Layout';
 export default function Index() {
   return (
     <Layout>
-      <h1 className="hidden">ytakhs.com</h1>
-      <div className="flex justify-start py-8">
-        <figure className="block px-2 m-auto w-full max-w-[30%] h-full max-h-[30%]">
+      <h1 style={{ display: 'none' }}>ytakhs.com</h1>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'flex-start',
+          gap: '1rem',
+          padding: '2rem 0',
+        }}
+      >
+        <figure
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            padding: '0 0.5rem',
+            maxWidth: '30%',
+            maxHeight: '30%',
+          }}
+        >
           <img
-            className="static rounded-full border border-slate-800"
+            style={{
+              borderRadius: '9999px',
+              borderWidth: '1px',
+              borderColor: 'rgb(30 41 59)',
+            }}
             src="/images/icon.svg"
             alt="icon"
           />
         </figure>
-        <div className="px-4 m-auto w-full">
+        <div>
           <H2 style={{ padding: 0 }}>About me</H2>
-          <ul className="p-4">
-            <li className="tracking-wider leading-relaxed">
+          <ul
+            style={{
+              padding: '1rem',
+              letterSpacing: '0.1rem',
+              lineHeight: '2rem',
+            }}
+          >
+            <li>
               GitHub:{' '}
               <a
                 href="https://github.com/ytakhs"
@@ -29,7 +55,7 @@ export default function Index() {
                 ytakhs
               </a>
             </li>
-            <li className="tracking-wider leading-relaxed">
+            <li>
               Twitter:{' '}
               <a
                 href="https://twitter.com/ytakhs"
@@ -42,10 +68,21 @@ export default function Index() {
             </li>
           </ul>
           <H2 style={{ padding: 0 }}>Content</H2>
-          <ul className="p-4">
-            <li className="tracking-wider leading-relaxed">
+          <ul
+            style={{
+              padding: '1rem',
+              letterSpacing: '0.1rem',
+              lineHeight: '1.5rem',
+            }}
+          >
+            <li>
               Writings:{' '}
-              <Link to="/entries" className="underline ">
+              <Link
+                to="/entries"
+                style={{
+                  textDecorationLine: 'underline',
+                }}
+              >
                 /entries
               </Link>
             </li>

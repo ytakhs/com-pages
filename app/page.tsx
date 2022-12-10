672415d56fd21126065c5892aec5d2ddaea24a12
ytakhs <21127627+ytakhs@users.noreply.github.com>
2022-12-11 00:04:04 +0900

    Add Sidebar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import { Header } from "../src/components/Header";
+import { Sidebar } from "../src/components/Sidebar";
 
 export default function Home() {
   return (
-    <div>
-      <Header />
+    <div className="flex">
+      <Sidebar />
+      <div>
+        <Header />
+      </div>
     </div>
   );
 }

52ffc441a2819f2d9f44a6081527538f6c70e568
ytakhs <21127627+ytakhs@users.noreply.github.com>
2022-12-10 12:22:15 +0900

    setup nextjs

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from "react";
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <head />
+      <body>{children}</body>
+    </html>
+  );
+}

8baa44a942263ec36f81c0e1c85017be3b12d81c
ytakhs <21127627+ytakhs@users.noreply.github.com>
2022-12-29 08:28:59 +0900

    Modify list style

--- src/features/writing/routes/WritingListPage.tsx
+++ src/features/writing/routes/WritingListPage.tsx
@@ -1,5 +1,6 @@
-import Link from "next/link";
 import { FC } from "react";
+import Link from "next/link";
+import { parseISO, format } from "date-fns";
 import { Layout } from "../../../components/Layout";
 import { Writing } from "../type";
 
@@ -11,15 +12,25 @@ export const WritingListPage: FC<Props> = ({ writings }) => {
   return (
     <Layout>
       <ul>
-        {writings.map(({ path: { date, slug } }) => (
-          <li key={`${date}-${slug}`}>
-            <Link href={`/writings/${date}/${slug}`}>
-              <div className="border-b py-4 text-lg font-bold hover:text-gray-400">
-                {slug}
-              </div>
-            </Link>
-          </li>
-        ))}
+        {writings.map(
+          ({ path: { date, slug }, frontmatter: { title, createdAt } }) => (
+            <li key={`${date}-${slug}`}>
+              <Link href={`/writings/${date}/${slug}`}>
+                <div className="flex justify-between gap-2 border-b py-4 text-lg font-bold hover:text-gray-400">
+                  <p className="w-full grow overflow-hidden text-ellipsis whitespace-nowrap">
+                    {title}
+                  </p>
+                  <time
+                    dateTime={format(parseISO(createdAt), "yyyy-MM-dd")}
+                    className="w-fit whitespace-nowrap text-sm font-normal"
+                  >
+                    {format(parseISO(createdAt), "MMM dd, yyyy")}
+                  </time>
+                </div>
+              </Link>
+            </li>
+          )
+        )}
       </ul>
     </Layout>
   );

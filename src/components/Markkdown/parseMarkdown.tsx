813532e7a39d68c4b82d2cc897e17584c08197f7
ytakhs <21127627+ytakhs@users.noreply.github.com>
2022-12-30 22:44:16 +0900

    Fix to use index as key

--- src/components/Markkdown/parseMarkdown.tsx
+++ src/components/Markkdown/parseMarkdown.tsx
@@ -14,13 +14,13 @@ const hasTokens = (tok: unknown): tok is { tokens: marked.Token[] } => {
 };
 
 export const parseMarkdown = (tokens: marked.Token[]): ReactNode[] => {
-  return tokens.map((token) => {
+  return tokens.map((token, i) => {
     switch (token.type) {
       case "blockquote": {
         return (
           <blockquote
             className="ml-2 border-l-4 border-gray-400 py-2 pl-4"
-            key={token.raw}
+            key={i}
           >
             {parseInline(token.tokens)}
           </blockquote>
@@ -28,7 +28,7 @@ export const parseMarkdown = (tokens: marked.Token[]): ReactNode[] => {
       }
       case "code": {
         return (
-          <div key={token.raw}>
+          <div key={i}>
             <SyntaxHighlight language={token.lang} PreTag="pre">
               {token.text}
             </SyntaxHighlight>
@@ -44,7 +44,7 @@ export const parseMarkdown = (tokens: marked.Token[]): ReactNode[] => {
               <h1
                 className="inline-flex gap-2 py-2 text-3xl font-bold"
                 id={encodeURI(token.text)}
-                key={token.raw}
+                key={i}
               >
                 <a href={`#${encodeURI(token.text)}`}>#</a>
                 {inline}
@@ -55,7 +55,7 @@ export const parseMarkdown = (tokens: marked.Token[]): ReactNode[] => {
               <h2
                 className="inline-flex gap-2 py-2 text-2xl font-bold"
                 id={encodeURI(token.text)}
-                key={token.raw}
+                key={i}
               >
                 <a href={`#${encodeURI(token.text)}`}>#</a>
                 {inline}
@@ -63,25 +63,25 @@ export const parseMarkdown = (tokens: marked.Token[]): ReactNode[] => {
             );
           case 3:
             return (
-              <h3 className="py-2 text-xl font-bold" key={token.raw}>
+              <h3 className="py-2 text-xl font-bold" key={i}>
                 {inline}
               </h3>
             );
           case 4:
             return (
-              <h4 className="py-2 text-lg font-bold" key={token.raw}>
+              <h4 className="py-2 text-lg font-bold" key={i}>
                 {inline}
               </h4>
             );
           case 5:
             return (
-              <h5 className="py-2 text-base font-bold" key={token.raw}>
+              <h5 className="py-2 text-base font-bold" key={i}>
                 {inline}
               </h5>
             );
           case 6:
             return (
-              <h6 className="py-2 text-base font-bold" key={token.raw}>
+              <h6 className="py-2 text-base font-bold" key={i}>
                 {inline}
               </h6>
             );
@@ -90,41 +90,41 @@ export const parseMarkdown = (tokens: marked.Token[]): ReactNode[] => {
         }
       }
       case "hr": {
-        return <hr key={token.raw} />;
+        return <hr key={i} />;
       }
       case "html": {
         return token.text;
       }
       case "list": {
-        const itemList = token.items.map((item) => {
+        const itemList = token.items.map((item, i2) => {
           const task = item.task
             ? [
                 <input
                   type="checkbox"
                   disabled={true}
                   checked={item.checked}
-                  key={item.raw}
+                  key={`input-${i}-${i2}`}
                 />,
               ]
             : [];
 
           const content = [...task, ...parseMarkdown(item.tokens)];
           return (
-            <li className="ml-5 list-disc leading-8" key={item.raw}>
+            <li className="ml-5 list-disc leading-8" key={`list-${i}-${i2}`}>
               {content}
             </li>
           );
         });
 
         if (token.ordered) {
-          return <ol key={token.raw}>{itemList}</ol>;
+          return <ol key={i}>{itemList}</ol>;
         } else {
-          return <ul key={token.raw}>{itemList}</ul>;
+          return <ul key={i}>{itemList}</ul>;
         }
       }
       case "paragraph": {
         return (
-          <p className="leading-8" key={token.raw}>
+          <p className="leading-8" key={i}>
             {parseInline(token.tokens)}
           </p>
         );
@@ -133,18 +133,21 @@ export const parseMarkdown = (tokens: marked.Token[]): ReactNode[] => {
         return null;
       }
       case "table": {
-        const headerCells = token.header.map((item, i) => {
+        const headerCells = token.header.map((item, i2) => {
           return (
-            <th align={token.align[i] || "justify"} key={i}>
+            <th align={token.align[i2] || "justify"} key={`header-${i}-${i2}`}>
               {parseInline(item.tokens)}
             </th>
           );
         });
-        const bodyRows = token.rows.map((cells, index) => {
+        const bodyRows = token.rows.map((cells, i2) => {
           return (
-            <td key={index}>
-              {cells.map((cel, i) => (
-                <td align={token.align[index] || "justify"} key={i}>
+            <td key={`data-${i}-${i2}`}>
+              {cells.map((cel, i3) => (
+                <td
+                  align={token.align[i2] || "justify"}
+                  key={`cell-${i}-${i2}-${i3}`}
+                >
                   {parseInline(cel.tokens)}
                 </td>
               ))}
@@ -171,7 +174,7 @@ export const parseMarkdown = (tokens: marked.Token[]): ReactNode[] => {
 };
 
 const parseInline = (tokens: marked.Token[]): ReactNode[] => {
-  return tokens.map((token) => {
+  return tokens.map((token, i) => {
     switch (token.type) {
       case "br": {
         return <br />;
@@ -182,17 +185,17 @@ const parseInline = (tokens: marked.Token[]): ReactNode[] => {
         return (
           <code
             className="text-amber-400 before:content-['`'] after:content-['`']"
-            key={token.raw}
+            key={i}
           >
             {unescapeText(text)}
           </code>
         );
       }
       case "del": {
-        return <del key={token.raw}>{parseInline(token.tokens)}</del>;
+        return <del key={i}>{parseInline(token.tokens)}</del>;
       }
       case "em": {
-        return <em key={token.raw}>{parseInline(token.tokens)}</em>;
+        return <em key={i}>{parseInline(token.tokens)}</em>;
       }
       case "escape": {
         return token.text;
@@ -204,7 +207,7 @@ const parseInline = (tokens: marked.Token[]): ReactNode[] => {
         const { href, text, title } = token;
 
         return (
-          <figure>
+          <figure key={i}>
             {/* eslint-disable-next-line @next/next/no-img-element */}
             <img src={href} alt={text} title={title} key={token.raw} />
           </figure>
@@ -219,16 +222,16 @@ const parseInline = (tokens: marked.Token[]): ReactNode[] => {
         }
 
         return (
-          <TextLink key={token.raw} href={href} {...props}>
+          <TextLink key={i} href={href} {...props}>
             {parseInline(token.tokens)}
           </TextLink>
         );
       }
       case "paragraph": {
-        return <p key={token.text}>{parseInline(token.tokens)}</p>;
+        return <p key={i}>{parseInline(token.tokens)}</p>;
       }
       case "strong": {
-        return <strong key={token.raw}>{parseInline(token.tokens)}</strong>;
+        return <strong key={i}>{parseInline(token.tokens)}</strong>;
       }
       case "text": {
         return unescapeText(token.text);

58349130e48452bb193661e29cdd0213d36b88d3
ytakhs <21127627+ytakhs@users.noreply.github.com>
2023-01-06 15:55:28 +0900

    Fix style

--- src/features/writing/routes/WritingListPage.tsx
+++ src/features/writing/routes/WritingListPage.tsx
@@ -16,7 +16,7 @@ export const WritingListPage: FC<Props> = ({ writings }) => {
           ({ path: { date, slug }, frontmatter: { title, createdAt } }) => (
             <li key={`${date}-${slug}`}>
               <Link href={`/writings/${date}/${slug}`}>
-                <div className="flex justify-between gap-2 border-b py-4 text-lg font-bold hover:text-gray-400">
+                <div className="flex items-center justify-between gap-2 border-b py-4 text-lg font-bold hover:text-gray-400">
                   <p className="w-full grow overflow-hidden text-ellipsis whitespace-nowrap">
                     {title}
                   </p>
